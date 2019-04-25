addb26dc9ba6a9473c609248ef0c238e82d9732f
Francesca Chater <francescachater@gmail.com>
2019-04-25 14:05:43 +0100

    Add case statement for 3 and 5

--- lib/fizzbuzz_2.rb
+++ lib/fizzbuzz_2.rb
@@ -1,7 +1,26 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+
+divisible = fizz(number) || buzz(number)
+
+
+  case divisible
+  when 5
+    "buzz"
+  when 3
     "fizz"
   else
     number
+  end
+end
+
+def fizz(number)
+  if number % 3 == 0
+    return 3
+  end
+end
+
+def buzz(number)
+  if number % 5 == 0
+    return 5
   end
 end
--- spec/fizzbuzz_2_rspec.rb
+++ spec/fizzbuzz_2_rspec.rb
@@ -9,4 +9,8 @@ describe 'Fizzbuzz' do
   it 'fizzes at 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'buzzes at 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end 
 end
